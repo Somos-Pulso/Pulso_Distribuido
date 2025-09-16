4433d693c923e0e1331577e7448510a0f25e7fdc
arrudaasaph <asapharruda@gmail.com>
2025-09-16 14:09:59 -0300

    :tada: init: commit inicial

---
+++ backend/pulso/src/main/java/web/pulso/PulsoApplication.java
@@ -0,0 +1,13 @@
+package web.pulso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsoApplication.class, args);
+	}
+
+}
